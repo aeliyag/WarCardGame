54c4822a9aff6d2e7b63b315443bb25a02493a56
aeliyag <aeliyagrover88@gmail.com>
2024-08-25 13:21:38 -0700

    Add files via upload

---
+++ War Card Game/War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by 24AeliyaGrover on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
